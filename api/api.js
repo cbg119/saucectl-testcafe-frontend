74d394d99a57301b03cf1a34e1200287f2640191
Christian <cbagdon119@gmail.com>
2024-10-04 16:55:13 -0500

    Use spawn() to stream output data

--- api/api.js
+++ api/api.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const { exec, spawn } = require("child_process")
 
+//SET THIS TO YOUR TESTCAFE/SAUCECTL FOLDER
 const TEST_CAFE_PATH = "/Users/christian/Desktop/dev/saucectl-testcafe-frontend/testcafe"
 
 const app = express()
@@ -10,9 +11,20 @@ app.use(express.json())
 
 app.post("/run-tests", async (req, res) => {
     console.log("Running tests...")
+
+    //exec() buffers output, this is not what we want.
+    //To extract job id/details in realtime, we should use spawn().
+    /*
     exec("saucectl run", { cwd: TEST_CAFE_PATH }, (error, stdout, stderr) => {
         res.send(stdout)
     })
+    */
+
+    const run_command = spawn("saucectl", ["run"], { cwd: TEST_CAFE_PATH })
+
+    run_command.stdout.on("data", data => res.write(data))
+    run_command.stderr.on("data", data => res.write(data))
+    run_command.on("close", _ => res.end())
 })
 
 app.listen(PORT, _ => {
