e266c9a01c487d1105f620192de9a4b8651e6298
Christian <cbagdon119@gmail.com>
2024-10-04 17:51:11 -0500

    Filter out job URLs from saucectl

--- api/api.js
+++ api/api.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const { exec, spawn } = require("child_process")
+const { url } = require("inspector")
 
 //SET THIS TO YOUR TESTCAFE/SAUCECTL FOLDER
 const TEST_CAFE_PATH = "/Users/christian/Desktop/dev/saucectl-testcafe-frontend/testcafe"
+
+HTTPS_REGEX = /https:\/\/[^\s/$.?#].[^\s]*/gi
 
 const app = express()
 const PORT = 3001
@@ -10,7 +13,7 @@ const PORT = 3001
 app.use(express.json())
 
 app.post("/run-tests", async (req, res) => {
-    console.log("Running tests...")
+    console.log("### RUNNING TESTS... ###")
 
     //exec() buffers output, this is not what we want.
     //To extract job id/details in realtime, we should use spawn().
@@ -22,9 +25,25 @@ app.post("/run-tests", async (req, res) => {
 
     const run_command = spawn("saucectl", ["run"], { cwd: TEST_CAFE_PATH })
 
-    run_command.stdout.on("data", data => res.write(data))
-    run_command.stderr.on("data", data => res.write(data))
-    run_command.on("close", _ => res.end())
+    run_command.stdout.on("data", (data) => {
+        if (data.includes("Suite started.")) {
+            //Parse string for test URL, can be further parsed for job ID
+            test_urls = (String(data)).match(HTTPS_REGEX)
+            console.log("Started a test - " + test_urls[0])
+        }
+
+        if (data.includes("Suite finished.")) {
+            //Parse string for test URL, can be further parsed for job ID
+            test_urls = (String(data)).match(HTTPS_REGEX)
+            console.log("Finished a test - " + test_urls[0])
+        }
+    })
+    //should be implemented later
+    //run_command.stderr.on("data", data => res.write(data))
+    run_command.on("close", _ => {
+        console.log("### TESTS HAVE ENDED ###")
+        res.end()
+    })
 })
 
 app.listen(PORT, _ => {
